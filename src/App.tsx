e0765a6566f5360511b8c54e12b66664d73fed4e
de-scientist <gitaumark502@gmail.com>
2025-11-11 14:36:05 +0300

    add function header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+function Header () {
+  return (
+    <header>
+      <a href="h" target="_blank" rel="noopener noreferrer"></a>
+    </header>
+  )
+}
+
 function App() {
   return (
     <div>
-      
+
     </div>
   )
   
