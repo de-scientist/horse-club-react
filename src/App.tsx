1c5f830117872aa4f531e4b63ba3b515fca1ec0f
de-scientist <gitaumark502@gmail.com>
2025-11-11 14:39:54 +0300

    create a navbar in the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 function Header () {
   return (
     <header>
-      <a href="h" target="_blank" rel="noopener noreferrer"></a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        HOME
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        ABOUT US
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        SERVICES
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        WHY US
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        TEAM
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        BLOG
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        TESTIMONIAL
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        GALLERY
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        CONTACT US
+      </a>
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        FAQ
+      </a>
     </header>
   )
 }
