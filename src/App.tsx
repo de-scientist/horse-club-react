91275e218c60da3dc01490b7e5effd59bd2ce654
de-scientist <gitaumark502@gmail.com>
2025-11-11 21:28:53 +0300

    import the servicessection component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import ServicesSection from "./components/Services";
+
 function App() {
   return (
     <div>
       <Header />
       <Hero />
       <About />
+      <ServicesSection />
     </div>
   );
 }
