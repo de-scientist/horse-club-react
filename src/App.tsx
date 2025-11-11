2556bea671f8e51b52f6b34d2b222eda6c1c2962
de-scientist <gitaumark502@gmail.com>
2025-11-11 21:32:11 +0300

    import reasonSection component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import ServicesSection from "./components/Services";
+import ReasonsSection from "./components/Reasons";
+import TeamSection from "./components/Team";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <Hero />
       <About />
       <ServicesSection />
+      <ReasonsSection />
+      <TeamSection />
+      
     </div>
   );
 }
