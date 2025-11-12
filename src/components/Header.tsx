11214bb0e9770b18134591d1296888f640450afb
de-scientist <gitaumark502@gmail.com>
2025-11-12 11:30:44 +0300

    update images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -88,7 +88,7 @@ const Header: React.FC<HeaderProps> = ({
               <li className="nav-list-item" key={index}>
                 <a
                   href={link.href}
-                  target="_blank"
+                  target="#"
                   rel="noopener noreferrer"
                   className="nav-list-item-link"
                 >
